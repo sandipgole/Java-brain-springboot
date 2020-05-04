610ecffa3cc052995a41bc6d26e77390534ed417
sandipgole <golesandip98@gmail.com>
2020-05-04 21:13:49 +0545

    feature added

--- src/main/java/com/HelloWorld/Helloworld/controller/Home.java
+++ src/main/java/com/HelloWorld/Helloworld/controller/Home.java
@@ -10,5 +10,11 @@ public class Home {
     public String sayHi()
         {
         return "HI stay home stay safe.....";
+
+    }
+    @RequestMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello.....";
     }
 }
