74995ee5bfd7762c20b14ebeae6eaa18a015159c
sandipgole <golesandip98@gmail.com>
2020-05-04 20:39:58 +0545

    first commit

---
+++ src/main/java/com/HelloWorld/Helloworld/controller/Home.java
@@ -0,0 +1,14 @@
+package com.HelloWorld.Helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping("/")
+    public String sayHi()
+        {
+        return "HI stay home stay safe.....";
+    }
+}
