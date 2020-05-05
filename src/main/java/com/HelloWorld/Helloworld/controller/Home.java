a437a14c71d70d9100fae24562f6e865332e5766
sandipgole <golesandip98@gmail.com>
2020-05-05 19:03:26 +0545

    third commit

--- src/main/java/com/HelloWorld/Helloworld/controller/Home.java
+++ src/main/java/com/HelloWorld/Helloworld/controller/Home.java
@@ -1,20 +1,30 @@
 package com.HelloWorld.Helloworld.controller;
-
+import com.HelloWorld.Helloworld.model.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class Home {
-
     @RequestMapping("/")
     public String sayHi()
         {
         return "HI stay home stay safe.....";
-
     }
     @RequestMapping("/hello")
     public String sayHello()
     {
         return "Hello.....";
+    }
+    @RequestMapping("/topics")
+    public List<demo> getAllTopics()
+    {
+        return Arrays.asList(
+                new demo("1","Java","programming language"),
+                new demo("2","python","programming language"),
+                new demo("3","JS","Scripting language"),
+                new demo("4","HTML","markup language")
+        );
     }
 }
---
+++ src/main/java/com/HelloWorld/Helloworld/model/demo.java
@@ -0,0 +1,40 @@
+package com.HelloWorld.Helloworld.model;
+
+public class demo {
+    private String id;
+    private String name;
+    private String desc;
+
+    public demo() {
+    }
+
+    public demo(String id, String name, String desc) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
